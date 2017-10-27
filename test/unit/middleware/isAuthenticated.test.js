65450187c8ba77cd02b02f3e59a212fdcb1740ca
stajics <stajic.srdjan92@gmail.com>
2017-10-27 12:38:20 +0100

    Added isAtuhenticated middleware test

---
+++ test/unit/middleware/isAuthenticated.test.js
@@ -0,0 +1,27 @@
+require('../../index');
+const isAuthenticated = require('../../../server/middlewares/isAuthenticated.midleware');
+
+const mockUnauthorized = jest.fn(err => err);
+const mockNext = jest.fn();
+const res = {};
+res.unauthorized = mockUnauthorized;
+
+describe(':isAuthenticated', () => {
+  jestTest('Should authenticate', async (done) => {
+    await isAuthenticated({ headers: { authorization: `Bearer ${fixtures.users[0].token}` } }, res, mockNext);
+    expect(mockNext.mock.calls.length).toBe(1);
+    done();
+  });
+
+  jestTest('Should error (missing Authorization header)', async (done) => {
+    await isAuthenticated({ headers: {} }, res);
+    expect(mockUnauthorized.mock.calls.length).toBe(1);
+    done();
+  });
+
+  jestTest('Should error (bad token)', async (done) => {
+    await isAuthenticated({ headers: { authorization: 'Bearer badToken' } }, res);
+    expect(mockUnauthorized.mock.calls.length).toBe(2);
+    done();
+  });
+});
