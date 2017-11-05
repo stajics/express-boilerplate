e7b769de6ea0c32e722b7aef03a1450d3d447074
igor <igorstajic273@gmail.com>
2017-11-05 19:18:00 +0100

    Update default error responses.

--- server/responses/notFound.response.js
+++ server/responses/notFound.response.js
@@ -1,5 +1,3 @@
-const util = require('util');
-
 module.exports = (req, res) => (error) => {
   res.status(404);
   logger.error({
@@ -9,7 +7,6 @@ module.exports = (req, res) => (error) => {
     query: req.query,
     body: req.body,
     authorization: req.headers.authorization,
-    errors: util.inspect(error),
   });
   res.send({ error });
 };
